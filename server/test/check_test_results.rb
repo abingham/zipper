35fb946dd9973cca9972fd7d000e2914c9b968e6
Jon Jagger <jon@jaggersoft.com>
2017-03-05 15:51:53 +0000

    switch to histogram+benchmark in example test

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -101,7 +101,7 @@ table =
     [ 'duration(test)[s]',      test_duration,      '<=',   2 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
-    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 100 ],
+    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 150 ],
     [ 'hits_per_line(test)',    hits_per_line_test, '<=',  10 ],
     [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.2 ],
   ]
--- server/test/src/prometheus_test.rb
+++ server/test/src/prometheus_test.rb
@@ -1,4 +1,5 @@
 require_relative 'zipper_test_base'
+require 'benchmark'
 require 'prometheus/client'
 require 'prometheus/client/push'
 
@@ -6,19 +7,27 @@ class PrometheusTest < ZipperTestBase
 
   def self.hex_prefix; '014'; end
 
-  test 'AD1','smoke test' do
+  def log
+    @log ||= NullLogger.new(self)
+  end
+
+  test 'AD1','histogram benchmark example' do
     # http://www.rubydoc.info/gems/prometheus-client/0.4.2
     prometheus = Prometheus::Client.registry
     refute_nil prometheus
-    zip_counter = prometheus.counter(:zip_total, 'zip request counter')
-    refute_nil zip_counter
-    assert_equal 0, zip_counter.get
-    zip_counter.increment
-    assert_equal 1, zip_counter.get
+    zip_seconds = prometheus.histogram(:zip_seconds, 'zip_request_seconds')
+    refute_nil zip_seconds
 
-    r = Prometheus::Client::Push.new(
-      'my_job', 'instance_name', 'http://prometheus_pushgateway:9091').replace(prometheus)
+    zip_seconds.observe({}, Benchmark.realtime { zip(started_kata_args[0][0]) })
+    zip_seconds.observe({}, Benchmark.realtime { zip(started_kata_args[1][0]) })
+    zip_seconds.observe({}, Benchmark.realtime { zip(started_kata_args[2][0]) })
+
+    gateway = 'http://prometheus_pushgateway:9091'
+    job = 'zip'
+    instance = nil # '192.168.99.100'
+    r = Prometheus::Client::Push.new(job, instance, gateway).add(prometheus)
     refute_nil r
+    assert_equal '202', r.response.code
   end
 
 end
